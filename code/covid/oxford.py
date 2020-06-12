631759b3c70f38347a61d601f97774a4cb6d6796
J.E.K <jens.erik.koehler@gmail.com>
2020-06-12 13:50:51 +0200

    Class documentation added

--- code/covid/oxford.py
+++ code/covid/oxford.py
@@ -1,5 +1,24 @@
-"""
+"""Oxford class
 
+This class provides a `pandas` DataFrame object of the Oxford Covid-19
+Government Response Tracker (OxCGRT) data, see README.md
+
+The DataFrame is loaded from local subdirectory, or, if not already existing,
+loaded from Oxford repository and processed into desired DataFrame.
+
+The processing steps are (see data_utils.py)
+(1) load Oxford data
+(2) init Hopkins data
+(3) merge Oxford with Hopkins data
+(4) extend data
+(5) fill missing values
+(6) add world population data
+(7) create features (stringency, mobility etc.)
+
+This class can be updated with the latest data from Oxford University see README.md
+
+NOTE:
+	This class is primarily used from Mobility class to update Oxford data.
 """
 
 import pandas as pd
@@ -13,11 +32,26 @@ OXFORD_CSV_PATH = os.path.join(FILE_PATH, "data/JEK_OxCGRT_v21_latest.csv")
 
 
 class Oxford:
+	"""
+	Attributes
+		----------
+		df : DataFrame
+			DataFrame object (pandas) that holds all feature columns.
+		min_date: str
+			Minimum or starting date of time series
+		max_date: str
+			Maximum or latest date of time series
+		num_date: int
+			Number of dates/days
+			
+		Methods
+		-------
+		save()
+			Save DataFrame to local subdirectory OXFORD_CSV_PATH
+		print_info()
+			Print some information about the DataFrame
+	"""
 	def __init__(self):
-		"""Initialization with the latest Oxford data.
-		The data is not ready for mobility model. Here we start
-		preparation of data.
-		"""
 		self.df = load_oxford_data()
 		self.min_date = self.df["Date"].unique().min()
 		self.max_date = self.df["Date"].unique().max()
