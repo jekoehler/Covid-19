26484d76e703bd5b20fabe0949368b69ba930a6d
J.E.K <66682313+jekoehler@users.noreply.github.com>
2020-06-12 11:31:15 +0200

    Method documentation added

--- code/covid/hopkins.py
+++ code/covid/hopkins.py
@@ -1,5 +1,25 @@
-"""
+"""Hopkins Class
 
+This class provides a `pandas` DataFrame object of the daily cumulative case numbers
+(starting Jan 22, 2020) reported by the Johns Hopkins University Center for Systems
+Science and Engineering (CSSE).
+
+The DataFrame is loaded from local subdirectory, or, if not already existing,
+loaded from JHU repository and processed into desired DataFrame.
+
+The processing steps are:
+(1) load global data
+(2) restructure data column-wise to row-wise
+(3) set country codes
+(4) prepare data (check provinces and NaN)
+(5) merge confirmed cases, confirmed deaths and recovered (all with different length)
+(6) expand dates backwards to 2020-01-01 (needed to have same starting date for other data)
+(7) save DataFrame to local subdirectory as csv
+
+This class can be updated with the latest data from Johns Hopkins University see README.md
+
+NOTE:
+	This class is primarily used from Mobility class to update Hopkins data.
 """
 import pandas as pd
 import numpy as np
@@ -25,11 +45,26 @@ US_URLS = {COL.cc: f"{BASE_URL}/time_series_covid19_confirmed_US.csv",
 
 
 class Hopkins:
-	
+	"""
+	Attributes
+	----------
+	data : DataFrame
+		DataFrame object (pandas) that holds all feature columns.
+	min_date: str
+		Minimum or starting date of time series
+	max_date: str
+		Maximum or latest date of time series
+	num_date: int
+		Number of dates/days of complete time series
+		
+	Methods
+	-------
+	update()
+		Update DataFrame with the latest data from Hopkins repository
+	save()
+		Save DataFrame to local subdirectory MOBILITY_CSV_PATH
+	"""
 	def __init__(self):
-		"""Initialize Hopkins class and load dataset from local source, otherwise
-		load data from Hopkins repository and prepare and save the data as pandas's
-		DataFrame."""
 		try:
 			print("Load Hopkins data...")
 			self.data = pd.read_csv(HOPKINS_CSV_PATH)
@@ -51,12 +86,12 @@ class Hopkins:
 		"""Load Hopkins data from repository and prepare and save the data
 		as pandas's DataFrame"""
 		print("- update Hopkins data...")
-		self.data = {case: load_global_data(case, url) for case, url in GLOBAL_URLS.items()}
-		self.data = prepare_and_merge(self.data)
+		self.data = {case: _load_global_data(case, url) for case, url in GLOBAL_URLS.items()}
+		self.data = _prepare_and_merge(self.data)
 		
 		self.data["DateTime"] = pd.to_datetime(self.data["Date"], format="%Y-%m-%d", errors="ignore")
 		self.data['Date'] = self.data['Date'].apply(lambda x: x.strftime("%Y-%m-%d"))
-		self.data = expand_dates(self.data)
+		self.data = _expand_dates(self.data)
 		
 		self.data = self.data[['Date', 'Country_Code', 'ConfirmedCases', 'ConfirmedDeaths', 'Recovered']]
 		self.min_date = self.data["Date"].unique().min()
@@ -71,12 +106,22 @@ class Hopkins:
 		print("- saved at:", HOPKINS_CSV_PATH)
 
 
-def prepare_and_merge(data):
+def _prepare_and_merge(data):
 	"""Prepare data of three different sources (confirmed cases,
 	confirmed deaths and recovered). These three sources has different
-	length and some countries doesn't have cumulated values (they have
+	length and some countries doesn't have accumulated values (they have
 	values for provinces instead, so before merging all three sources we
 	need to compute the cumulative values for a country).
+	
+	Parameter
+	---------
+	data: DataFrame
+		Original, raw data from JHU repository (row-wise time-series)
+		
+	Returns
+	-------
+	data: DataFrame
+		Merged data of column-wise time-series
 	"""
 	print("- prepare and merge data... ")
 	# reorder columns
@@ -85,9 +130,8 @@ def prepare_and_merge(data):
 		temp = data[case]
 		temp = temp[new_cols]
 		
-		days = temp["Date"].nunique()
 		# get all countries having some provinces
-		provs = check_provinces(temp, days)
+		provs = _check_provinces(temp)
 		# check countries without nan at Province_State
 		countries_without_nan = []
 		for p in provs:
@@ -97,7 +141,7 @@ def prepare_and_merge(data):
 		
 		collected_dfs = [temp]
 		for c in countries_without_nan:
-			nan_df = compute_nan(temp, c, case)
+			nan_df = _compute_nan(temp, c, case)
 			collected_dfs.append(nan_df)
 		
 		temp = pd.concat(collected_dfs)
@@ -107,7 +151,7 @@ def prepare_and_merge(data):
 		temp = temp.drop(columns=["Province_State"])
 		data[case] = temp
 	
-	# merge all three different dataframes
+	# merge all three different data frames
 	new_df = data[COL.cc]
 	new_df = new_df.merge(data[COL.cd],
 	                      left_on=['Date', 'Country_Code', 'Country_Region'],
@@ -117,8 +161,18 @@ def prepare_and_merge(data):
 	return new_df
 
 	
-def check_provinces(data, days):
+def _check_provinces(data):
 	"""Helper function to check which country has values for provinces.
+	
+	Parameter
+	---------
+	data: DataFrame
+		DataFrame of Hopkins class
+		
+	Returns
+	-------
+	country codes: list
+		List of country codes of countries having some provinces
 	"""
 	def check_number_of_entries(data, code):
 		parts = data[(data.Country_Code == code)]
@@ -127,7 +181,7 @@ def check_provinces(data, days):
 	country_codes = data['Country_Code'].unique()
 	
 	prov_countries = {}
-	
+	days = data["Date"].nunique()
 	for code in country_codes:
 		num = check_number_of_entries(data, code)
 		if num > days:
@@ -136,32 +190,58 @@ def check_provinces(data, days):
 	return prov_countries.keys()
 	
 
-def compute_nan(data, ccode, case):
+def _compute_nan(data, c_code, case) -> pd.DataFrame:
 	"""Helper function to compute the cumulative values from
 	all provinces to be set as the countries values for the
 	provinces NaN values.
+	
+	Parameters
+	----------
+	data: DataFrame
+		DataFrame of Hopkins class
+	c_code: str
+		Country code of interest
+	case: str
+		String of case (ConfirmedCases, ConfirmedDeaths, Recovered)
+		
+	Returns
+	-------
+	new_df: DataFrame
+		New DataFrame for each country and case with NaN set to Province_State
 	"""
-	lookup = (data.Country_Code == ccode)
+	lookup = (data.Country_Code == c_code)
 	country = data[lookup]
 	new_nan = country.groupby('Date')[case].sum()
 	
 	new_df = pd.DataFrame({'Date': new_nan.index, case: new_nan.values})
 	new_df['Province_State'] = np.nan
 	new_df['Country_Region'] = country['Country_Region'].unique()[0]
-	new_df['Country_Code'] = ccode
+	new_df['Country_Code'] = c_code
 	
 	return new_df
 
 
-def load_global_data(cases, path_url):
+def _load_global_data(cases, path_url) -> pd.DataFrame:
 	"""Load global data from Hopkins database at github (see URLs).
 	These database has dates column-wise. We need to restructure the
-	table to have dates row-wise
+	table to have dates row-wise.
+	
+	Parameters
+	----------
+	cases: str
+		String of case (ConfirmedCases, ConfirmedDeaths, Recovered)
+	path_url: str
+		URL of JHU repository for the corresponding time series
+		
+	Returns
+	-------
+	data: DataFrame
+		Restructured and extended DataFrame
 	"""
 	print("- load data for", cases)
 	data = pd.read_csv(path_url, ',')
 	# reorganize dates
-	data = restructure(data=data, idx=4, cases=cases)
+	data = _restructure(data=data, idx=4, cases=cases)
 	# rename data
 	data = data.rename(columns={'Country/Region': 'Country_Region',
 	                            'Province/State': 'Province_State'})
@@ -173,14 +253,28 @@ def load_global_data(cases, path_url):
 	                        ignore_index=True)
 	
 	# add country code
-	data = set_hopkins_country_code(data=data)
+	data = _set_hopkins_country_code(data=data)
 	
 	return data
 
 
-def restructure(data, idx, cases):
+def _restructure(data, idx, cases):
 	"""The raw downloaded data has dates column wise, not row wise.
 	So we need to reorganize data to have dates in rows.
+	
+	Parameters
+	----------
+	data: DataFrame
+		DataFrame of Hopkins class
+	idx: int
+		Column index that divides feature columns from time-series columns
+	cases: str
+		String of case (ConfirmedCases, ConfirmedDeaths, Recovered)
+		
+	Returns
+	-------
+	data: DataFrame
+		Restructured (row-wise time-series) DataFrame
 	"""
 	print('-- restructure date columns to rows...')
 	cols = data.columns.to_list()
@@ -200,8 +294,18 @@ def restructure(data, idx, cases):
 	return data
 
 
-def set_hopkins_country_code(data):
-	"""Set additional country code ISO 3166-1 Alpha.
+def _set_hopkins_country_code(data):
+	"""Set additional country code ISO 3166-1 Alpha-3.
+	
+	Parameters
+	----------
+	data: DataFrame
+		DataFrame of Hopkins class
+		
+	Returns
+	-------
+	data: DataFrame
+		Extended DataFrame including country codes
 	"""
 	print('-- set country codes...')
 	# load ISO country names and codes
@@ -249,7 +353,7 @@ def set_hopkins_country_code(data):
 	           'GAZ': 'West Bank and Gaza'
 	           }
 	
-	#
+	# update country codes
 	for c, n in zip(kd_dict.keys(), kd_dict.values()):
 		# print('set code {0} for {1}'.format(c, n))
 		data.loc[(data['Country_Region'] == n), 'Country_Code'] = c
@@ -257,15 +361,25 @@ def set_hopkins_country_code(data):
 	# because of different writings
 	data.loc[(data['Country_Region'] == "Cote d'Ivoire"), 'Country_Code'] = 'CIV'
 	data['Country_Code'].fillna(-1, inplace=True)
-	# finally check missing values
+	# TODO: (debug) finally check missing values
 	# check_missing_values(data)
 	
 	return data
 
 
-def expand_dates(data):
+def _expand_dates(data):
 	"""Expand date period back to 2020-01-01, because we use need to
 	merge with Oxford data which starts at 2020-01-01.
+	
+	Parameters
+	----------
+	data: DataFrame
+		DataFrame of Hopkins class
+		
+	Returns
+	-------
+	data: DataFrame
+		Extended DataFrame with time-series starting at 2020-01-01
 	"""
 	print('- expand dates...')
 	# collect new entries
